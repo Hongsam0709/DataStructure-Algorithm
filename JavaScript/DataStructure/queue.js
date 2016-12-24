45e43a6c2f90922e2173f2a1906b622daa1aa743
su0709 <seekers0709@gmail.com>
2016-12-24 15:32:21 +0900

    queue complete

--- JavaScript/DataStructure/queue.js
+++ JavaScript/DataStructure/queue.js
@@ -1,8 +1,34 @@
 var QueueModule = (function QueueModule() {
 
-    function Queue(){
-
+    function Queue() {
+        this.repository = [];
     }
 
-    Queue.prototype.
+    Queue.prototype.put = function put(_array) {
+        this.repository.push(_array);
+    };
+
+    Queue.prototype.get = function get() {
+        if (this.repository.length === 0) {
+            console.log('repository is empty.');
+            return null;
+        }
+        var result = this.repository[0];
+        this.repository = this.repository.slice(1);
+        return result;
+    };
+
+    Queue.prototype.display = function display() {
+        var result = this.repository.toString();
+        console.log(result);
+    };
+
+    var init = function init() {
+        var object = new Queue();
+        return object;
+    };
+
+    return {
+        init: init
+    };
 })();
